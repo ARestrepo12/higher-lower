803c9e7891440df5f392d70d4f3dcd1a8ffdd0d9
unknown <arestre@ncsu.edu>
2023-04-08 11:22:13 -0400

    all criteria met besides duplicate

--- higher-lower.js
+++ higher-lower.js
@@ -1,42 +1,51 @@
-
-
-function maxnumber() {
-let range = prompt("please enter the maximum number you want to guess between.");
-    if (isNaN(range)) {
-        alert("That is not a number please try again");
-        return maxnumber();
+let maxNum = 0
+while (true) {
+    let enterNum = prompt('Plese enter the maximum number you want to guess between.')
+    maxNum = Math.round(parseFloat(enterNum))
+    if (!isNaN(maxNum) && maxNum > 0) {
+        break
     }
-    else if (range <= 2) {
-        alert("Please enter a larger range");
-        return maxnumber();
-    }
-    else {
-        return range;
-    }
+    alert('Please enter a positive number')
 }
-maxnumber();
-console.log (range);
 
+let num = maxNum
 
-let num = Math.floor(Math.random() * 20) +1;
+let message2 = document.getElementById('message2')
+message2.innerHTML = `Guess a number between 1 and ${num}`
 
-console.log(num);
+let randomNum = Math.floor(Math.random() * num) + 1
 
+let countGuess = 0
+let count = []
 
+let rightNum = false
+while (!rightNum) {
+    const guessBtn = document.getElementById('guessBtn')
+    guessBtn.addEventListener('click', do_guess)
 
+    function do_guess() {
+        let inputguess = document.getElementById('guess').value
+        let guess1 = Math.round(Number(inputguess))
+        let message = document.getElementById('message')
 
-function do_guess() {
-    let guess = Number(document.getElementById("guess").value);
-
-    let message= document.getElementById("message");
-
-    if(guess == num) {
-        message.innerHTML = "You got it!";
+        if(isNaN(guess1)) {
+            message.innerHTML = 'That input is not a number.'
+        } else if (guess1 > num || guess1 < 1) {
+            message.innerHTML = 'That number is not in range, please try again.'
+        } else if (guess1 >= 1 && guess1 <= num) {
+            count.push(guess1)
+            countGuess++
+            if (guess1 != randomNum) {
+                const hint = 
+                guess1 > randomNum ? ' a lower number' : ' a higher number'
+                message.innerHTML = `try guessing ${hint}`
+            } else {
+                message.innerHTML = `Congratulations! you got it right (${guess1}), and it took you ${count.length} tries and your guesses were ${count}.`
+            }
+        } else {
+            return
+        }
     }
-    else if (guess > num) {
-        message.innerHTML = "No, try a lower number.";
-    }
-    else {
-        message.innerHTML = "No, try a higher number.";
-    }
+    rightNum = true
+
 }
